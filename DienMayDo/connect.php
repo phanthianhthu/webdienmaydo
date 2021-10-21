928727fa1d7e1546c8ae099d826abb91e5c34111
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-10-21 10:41:48 +0700

    add

--- DienMayDo/connect.php
+++ DienMayDo/connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://dauiapgreatbrq:41b2803fb1bd0bcc8c9d7149f3a4507c152f8703a0b854372da0070d59d2eee9@ec2-44-195-240-222.compute-1.amazonaws.com:5432/dcqh067a6mvd6j");
+	$Connect = pg_connect("postgres://jaayojojmjkvzn:84db5e35b35e0672c84ba4786a089db76f005a0c931490ee91a13d947fa3fda1@ec2-44-195-240-222.compute-1.amazonaws.com:5432/d6771km0h01h60");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
